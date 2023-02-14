72f7e77e2ab570ac094b72495bce0772d5057c4b
Pane.L <12314389+Whatssup-PaneL@user.noreply.gitee.com>
2023-02-14 15:50:42 +0800

    用户信息页面初始布局

--- src/pages/my/components/CollectGoods.vue
+++ src/pages/my/components/CollectGoods.vue
@@ -1,12 +1,362 @@
 <template>
-  
-</template>
-
+    <view class="masonry">
+        <template v-if="tabIndex === 0">
+            <view class="column">
+                <view class="card topic">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/topic_1.jpg"></image>
+                    <view class="name">忙里忙外，回家吃饭</view>
+                    <view class="price">19.9元<text>起</text></view>
+                    <view class="extra">
+                        <text class="icon-heart">1220</text>
+                        <text class="icon-preview">1000</text>
+                    </view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_6.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_6.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_8.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_7.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+            </view>
+            <view class="column">
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_5.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_7.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card topic">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/topic_2.jpg"></image>
+                    <view class="name">忙里忙外，回家吃饭</view>
+                    <view class="price">19.9元<text>起</text></view>
+                    <view class="extra">
+                        <text class="icon-heart">1220</text>
+                        <text class="icon-preview">1000</text>
+                    </view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_5.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_3.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+            </view>
+        </template>
+        <template v-if="tabIndex === 1">
+            <view class="column">
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_7.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_8.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card topic">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/topic_1.jpg"></image>
+                    <view class="name">忙里忙外，回家吃饭</view>
+                    <view class="price">19.9元<text>起</text></view>
+                    <view class="extra">
+                        <text class="icon-heart">1220</text>
+                        <text class="icon-preview">1000</text>
+                    </view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_6.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_6.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+            </view>
+            <view class="column">
+                <view class="card topic">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/topic_2.jpg"></image>
+                    <view class="name">忙里忙外，回家吃饭</view>
+                    <view class="price">19.9元<text>起</text></view>
+                    <view class="extra">
+                        <text class="icon-heart">1220</text>
+                        <text class="icon-preview">1000</text>
+                    </view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_5.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_5.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_7.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_3.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+            </view>
+        </template>
+        <template v-if="tabIndex === 2">
+            <view class="column">
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_6.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_8.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card topic">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/topic_1.jpg"></image>
+                    <view class="name">忙里忙外，回家吃饭</view>
+                    <view class="price">19.9元<text>起</text></view>
+                    <view class="extra">
+                        <text class="icon-heart">1220</text>
+                        <text class="icon-preview">1000</text>
+                    </view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_6.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_7.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+            </view>
+            <view class="column">
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_7.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card topic">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/topic_2.jpg"></image>
+                    <view class="name">忙里忙外，回家吃饭</view>
+                    <view class="price">19.9元<text>起</text></view>
+                    <view class="extra">
+                        <text class="icon-heart">1220</text>
+                        <text class="icon-preview">1000</text>
+                    </view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_5.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+                <view class="card brand">
+                    <view class="locate">
+                        <text class="icon-locate"></text>中国
+                    </view>
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/brand_logo_5.jpg">
+                    </image>
+                    <view class="name">小米优购</view>
+                    <view class="alt">小米优购闪购嗨购</view>
+                </view>
+                <view class="card goods">
+                    <image mode="widthFix" src="http://static.botue.com/erabbit/static/uploads/goods_big_3.jpg"></image>
+                    <view class="name">彩色鹅卵石小清新防水防烫长方形餐桌圆桌布艺茶几垫电视柜盖布
+                        鹅软石桌布yg056</view>
+                    <view class="price">¥899</view>
+                </view>
+            </view>
+        </template>
+    </view>
+</template>  
 <script>
 export default {
+    props: ["tabIndex"],
+    created(){
+        console.log(this.tabIndex);
+    }
 };
 </script>
 
-<style >
+<style lang="scss">
+/* 瀑布流布局 */
+.masonry {
+    display: flex;
+    justify-content: space-between;
+    padding: 20rpx 0;
 
+    .column {
+        width: 345rpx;
+
+        .card {
+            padding: 20rpx 15rpx;
+            margin-bottom: 20rpx;
+            border-radius: 8rpx;
+            background-color: #fff;
+        }
+    }
+
+    .card {
+        .name {
+            font-size: 24rpx;
+            color: #333;
+            margin-top: 10rpx;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+        }
+
+        .price {
+            line-height: 1;
+            font-size: 22rpx;
+            color: #cf4444;
+        }
+
+        .locate {
+            text-align: left;
+            color: #999;
+            margin-bottom: 10rpx;
+            font-size: 24rpx;
+        }
+    }
+
+    .topic {
+        .price {
+            margin: 10rpx 0;
+
+            text {
+                color: #999;
+            }
+        }
+
+        .extra {
+            line-height: 1;
+            font-size: 22rpx;
+            color: #666;
+        }
+    }
+
+    .extra {
+        text {
+            margin-right: 14rpx;
+
+            &::before {
+                margin-right: 4rpx;
+            }
+        }
+    }
+
+    .icon-preview {
+        &:before {
+            font-size: 25rpx;
+        }
+    }
+
+    .goods {
+        .price {
+            margin-top: 10rpx;
+        }
+    }
+
+    .brand {
+        text-align: center;
+
+        .name {
+            margin: 10rpx 0 8rpx;
+        }
+
+        .alt {
+            line-height: 1;
+            color: #666;
+            font-size: 24rpx;
+        }
+    }
+}
 </style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,12 +1,271 @@
 <template>
-  
+    <scroll-view enhanced scroll-y id="scrollView">
+        <view class="viewport" :style="{ paddingTop: top + 'px' }">
+            <!-- 顶部背景 -->
+            <view class="navbar">
+                <view class="title">我的</view>
+            </view>
+            <!-- 个人资料 -->
+            <view class="profile">
+                <view class="overview">
+                    <navigator v-if="profile.token" url="/pages/my/profile" hover-class="none">
+                        <image mode="aspectFill" class="avatar" :src="profile.avatar"></image>
+                    </navigator>
+                    <!-- 未登录：点击头像跳转登录页 -->
+                    <navigator v-else url="/pages/login/index" hover-class="none">
+                        <image class="avatar" src="http://static.botue.com/erabbit/static/uploads/avatar_3.jpg"></image>
+                    </navigator>
+                    <view class="meta">
+                        <view @tap="goToProfile" v-if="profile" class="nickname">
+                            {{ profile.nickname }}
+                        </view>
+                        <!-- 未登录：点击文字跳转登录页 -->
+                        <navigator v-else url="/pages/login/index" hover-class="none" class="nickname">
+                            未登录
+                        </navigator>
+                        <view class="extra">
+                            <text v-if="!profile" class="tips">点击登录账号</text>
+                            <template v-else>
+                                <text class="update">更新头像昵称</text>
+                                <text class="relogin">切换账号</text>
+                            </template>
+                        </view>
+                    </view>
+                </view>
+                <navigator class="settings" url="/pages/my/settings" hover-class="none">
+                    设置
+                </navigator>
+            </view>
+            <!-- 订单 -->
+            <view class="orders">
+                <view class="title">
+                    我的订单
+                    <navigator url="/pages/order/index?type=0" hover-class="none">
+                        查看全部订单<text class="icon-right"></text>
+                    </navigator>
+                </view>
+                <view class="section">
+                    <navigator v-for="item in orderTypes" :key="item.text" :class="item.icon"
+                        :url="'/pages/order/index?type=' + item.type" hover-class="none">{{ item.text }}</navigator>
+                    <navigator class="icon-handset" url=" " hover-class="none">售后</navigator>
+                </view>
+            </view>
+            <!-- 部件 -->
+            <view class="widgets">
+                <view class="tabs">
+                    <text v-for="(item, index) in tabs" :key="item" :class="{ active: tabIndex === index }"
+                        @click="tabIndex = index">{{ item }}</text>
+                </view>
+            </view>
+            <!-- 商品列表 -->
+            <CollectGoods :tab-index="tabIndex"></CollectGoods>
+        </view>
+    </scroll-view>
 </template>
 
 <script>
+import CollectGoods from './components/CollectGoods.vue';
 export default {
+    components:{
+        CollectGoods
+    },
+    computed:{
+        top(){
+            return this.$store.state.buttonArea.top+this.$store.state.buttonArea.height
+        }
+    },
+    data() {
+        return {
+            orderTypes: [
+                { text: "待付款", icon: "icon-currency", type: 1 },
+                { text: "待发货", icon: "icon-gift", type: 2 },
+                { text: "待收货", icon: "icon-check", type: 3 },
+                { text: "待评价", icon: "icon-comment", type: 4 },
+            ],
+            tabs: ["我的收藏", "猜你喜欢", "我的足迹"],
+            tabIndex: 0,
+        }
+    },
 };
 </script>
 
-<style >
+<style lang="scss">
+page {
+    height: 100%;
+    overflow: hidden;
+    background-color: #f7f7f8;
+}
 
+#scrollView {
+    height: 100%;
+    overflow: hidden;
+}
+
+.viewport {}
+
+.navbar {
+    width: 750rpx;
+    height: 380rpx;
+    background-image: url(http://static.botue.com/erabbit/static/images/center_bg.png);
+    background-size: contain;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9;
+
+    .title {
+        height: 64rpx;
+        line-height: 64rpx;
+        text-align: center;
+        color: #fff;
+        font-size: 32rpx;
+        opacity: 0;
+        font-weight: bold;
+    }
+}
+
+.profile {
+    position: relative;
+    z-index: 99;
+
+    .overview {
+        display: flex;
+        width: 500rpx;
+        height: 120rpx;
+        padding: 0 36rpx;
+        color: #fff;
+
+        .avatar {
+            width: 120rpx;
+            height: 120rpx;
+            border-radius: 50%;
+        }
+
+        .meta {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: flex-start;
+            line-height: 30rpx;
+            padding: 16rpx 0;
+            margin-left: 20rpx;
+        }
+    }
+
+    .meta {
+        .nickname {
+            max-width: 180rpx;
+            margin-bottom: 16rpx;
+            font-size: 30rpx;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+
+        .extra {
+            display: flex;
+            font-size: 20rpx;
+        }
+
+        .tips {
+            font-size: 22rpx;
+        }
+    }
+
+    .settings {
+        position: absolute;
+        top: 50%;
+        right: 80rpx;
+        line-height: 1;
+        font-size: 30rpx;
+        color: #fff;
+        transform: translateY(-50%);
+    }
+}
+
+.profile .meta .update,
+.profile .meta .relogin {
+    padding: 3rpx 10rpx 1rpx;
+    color: rgba(255, 255, 255, 0.8);
+    border: 1rpx solid rgba(255, 255, 255, 0.8);
+    margin-right: 10rpx;
+    border-radius: 30rpx;
+}
+
+.orders {
+    position: sticky;
+    top: 140rpx;
+    z-index: 99;
+    padding: 30rpx;
+    margin: 50rpx 20rpx 0;
+    background-color: #fff;
+    border-radius: 10rpx;
+    box-shadow: 0 4rpx 6rpx rgba(240, 240, 240, 0.6);
+
+    .title {
+        height: 40rpx;
+        line-height: 40rpx;
+        font-size: 28rpx;
+        color: #1e1e1e;
+
+        navigator {
+            font-size: 24rpx;
+            color: #939393;
+            float: right;
+        }
+    }
+
+    .section {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        padding: 40rpx 20rpx 10rpx;
+
+        navigator {
+            text-align: center;
+            font-size: 24rpx;
+            color: #333;
+
+            &::before {
+                display: block;
+                font-size: 60rpx;
+                color: #ff9545;
+            }
+        }
+    }
+}
+
+/* 小部件 */
+.widgets {
+    padding: 20rpx 20rpx 0;
+    background-color: #f7f7f8;
+    position: relative;
+    z-index: 1;
+
+    .tabs {
+        display: flex;
+        justify-content: space-around;
+        height: 80rpx;
+        line-height: 80rpx;
+
+        text {
+            font-size: 28rpx;
+            color: #333;
+            position: relative;
+        }
+
+        .active {
+            &::after {
+                position: absolute;
+                left: 50%;
+                bottom: 12rpx;
+                width: 60rpx;
+                height: 4rpx;
+                background-color: #27ba9b;
+                content: "";
+                transform: translate(-50%);
+            }
+        }
+    }
+}
 </style>
